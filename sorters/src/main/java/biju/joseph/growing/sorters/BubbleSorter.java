52fc77d13edacffbd721d67d6fa8813ed89f68e6
colorshifter <biju.joseph.padupurackal@gmail.com>
2012-10-07 10:04:31 -0400

    #2 - Added a test suite

--- sorters/src/main/java/biju/joseph/growing/sorters/BubbleSorter.java
+++ sorters/src/main/java/biju/joseph/growing/sorters/BubbleSorter.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * An implementation of @see <a href="http://en.wikipedia.org/wiki/Merge_sort">Merge Sort</a>
+ * An simple and humble implementation of @see <a href="http://en.wikipedia.org/wiki/Bubble_sort">Bubble Sort</a>
  *
  * @author Biju Joseph
  *         Created on : 10/7/12 9:13 AM
@@ -21,14 +21,17 @@ public class BubbleSorter implements Sorter {
     @Override
     public <T extends Comparable> List<T> sort(List<T> list) {
         ArrayList<T> retList = new ArrayList<T>(list);
-        if (retList.isEmpty()) return retList;
+        if (retList.isEmpty()) return retList;  //empty list when the content is empty
 
         int length = list.size();
+
+        //making sure that element with lowest ordinal bubbles-up
         for (int i = 0; i < length; i++) {
             for (int j = i + 1; j < length; j++) {
                 T a = retList.get(i);
                 T b = retList.get(j);
 
+                //swap so that the weighted element sinks down
                 if (a.compareTo(b) > 0) {
                     retList.set(i, b);
                     retList.set(j, a);
---
+++ sorters/src/test/java/biju/joseph/growing/sorter/SorterTestSuite.java
@@ -0,0 +1,13 @@
+package biju.joseph.growing.sorter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Biju Joseph
+ * Created on : 10/7/12 8:23 AM
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BubbleSorterTest.class, MergeSorterTest.class})
+public class SorterTestSuite {
+}
